e06d1bff8f15232518207b742f4d087b757d09aa
Iker Ruiz de Infante Gonzalez <iker@irzinfante.dev>
2026-06-03 11:48:23 +0200

    Added component C6

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ git clone https://github.com/GrupoCiberataquesGrafosTiempoReal/data-producer.git
 git clone https://github.com/GrupoCiberataquesGrafosTiempoReal/data-enricher.git
 
 # Create persistency volumes
-mkdir -p .volumes/kafka
+mkdir -p .volumes/kafka .volumes/neo4j/{data,logs}
 
 # Download test data into data-producer
 curl -L "https://github.com/GrupoCiberataquesGrafosTiempoReal/.github/archive/main.tar.gz" | tar -xvz --strip-components=1 -C ./data-producer ".github-main/data"
