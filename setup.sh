d85962d16c95e48d31cd038f0392e2dd6e917a34
Iker Ruiz de Infante Gonzalez <iker@irzinfante.dev>
2026-06-22 10:28:23 +0200

    Added component C4

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ curl -L -O https://raw.githubusercontent.com/GrupoCiberataquesGrafosTiempoReal/.
 git clone https://github.com/GrupoCiberataquesGrafosTiempoReal/data-producer.git
 git clone https://github.com/GrupoCiberataquesGrafosTiempoReal/data-enricher.git
 git clone https://github.com/GrupoCiberataquesGrafosTiempoReal/data-modeler.git
+git clone https://github.com/GrupoCiberataquesGrafosTiempoReal/mitre-detector.git
 
 # Create persistency volumes
 mkdir -p .volumes/kafka .volumes/neo4j/{data,logs}
